13a0ac6fdbc830ceb9e79913bd80d094f5a5f867
Saikat <saikatpradhan296@gmail.com>
2026-01-03 09:36:00 +0530

    FEATURE: Implemented Razorpay Payement gateway integration

---
+++ frontend/src/components/data.js
@@ -0,0 +1,28 @@
+const productsData = [
+    {
+        id: 1,
+        title: "Soft Bag",
+        price: 600,
+        image: "./images/bag.jpg",
+    },
+    {
+        id: 2,
+        title: "Headphone",
+        price: 800,
+        image: "./images/headphone.jpg",
+    },
+    {
+        id: 3,
+        title: "Jacket",
+        price: 600,
+        image: "./images/jacket.jpg",
+    },
+    {
+        id: 4,
+        title: "Jeans",
+        price: 900,
+        image: "./images/jeans.png",
+    }
+]
+
+export default productsData;
